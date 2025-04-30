563e099479e6af8106920756d27d4652ef549763
Martial59110 <floquet.martial@gmail.com>
2025-04-30 12:33:53 +0200

    feat(users): add roles field to User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { News } from '../../news/entities/news.entity';
 import { TextDonation } from '../../text-donations/entities/text-donation.entity';
 import { TextVolunteer } from '../../text-volunteers/entities/text-volunteer.entity';
 import { ClothingExample } from '../../clothing-examples/entities/clothing-example.entity';
+import { Role } from '../../auth/enums/role.enum';
 
 export class User {
   id: string;
@@ -10,6 +11,7 @@ export class User {
   password: string;
   firstname?: string;
   lastname?: string;
+  roles: Role[];
   createdAt: Date;
   updatedAt: Date;
   
