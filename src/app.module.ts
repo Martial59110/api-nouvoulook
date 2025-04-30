e0f0ca283aac55cfb525810b126598901c66d955
Martial59110 <floquet.martial@gmail.com>
2025-04-30 11:51:34 +0200

    feat(app): add TextDonationsModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { RolesGuard } from './guards/roles.guard';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TextDonationsModule } from './text-donations/text-donations.module';
 
 @Module({
   imports: [
@@ -27,6 +30,9 @@ import { PrismaModule } from './prisma/prisma.module';
       limit: 10,
     }]),
     PrismaModule,
+    AuthModule,
+    UsersModule,
+    TextDonationsModule,
   ],
   controllers: [AppController],
   providers: [
