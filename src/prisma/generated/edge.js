74bc998342e5f0df685a72f26a5c86b38a7c62d0
Martial59110 <floquet.martial@gmail.com>
2025-05-02 11:56:17 +0200

    chore(prisma): update generated Prisma client files

--- src/prisma/generated/edge.js
+++ src/prisma/generated/edge.js
@@ -201,7 +201,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -211,12 +211,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://admin:mdp@localhost:5432/nouvoulook"
       }
     }
   },
