69510c7a648a7bd2f47548acbbb69d25b3214a06
Martial59110 <floquet.martial@gmail.com>
2025-05-10 14:19:31 +0200

    feat(clothing): update clothing examples controller

--- src/clothing-examples/clothing-examples.controller.ts
+++ src/clothing-examples/clothing-examples.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Query } from '@nestjs/common';
 import { ClothingExamplesService } from './clothing-examples.service';
 import { CreateClothingExampleDto } from './dto/create-clothing-example.dto';
 import { UpdateClothingExampleDto } from './dto/update-clothing-example.dto';
@@ -6,6 +6,7 @@ import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { Roles } from '../decorators/roles.decorator';
 import { RolesGuard } from '../guards/roles.guard';
 import { Role } from '../auth/enums/role.enum';
+import { Public } from 'src/decorators/public.decorator';
 @Controller('clothing-examples')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class ClothingExamplesController {
@@ -18,7 +19,14 @@ export class ClothingExamplesController {
   }
 
   @Get()
-  findAll() {
+  @Public()
+  findAll(@Query('accepted') accepted?: string) {
+    if (accepted === 'true') {
+      return this.clothingExamplesService.findMany({ accepted: true });
+    }
+    if (accepted === 'false') {
+      return this.clothingExamplesService.findMany({ accepted: false });
+    }
     return this.clothingExamplesService.findAll();
   }
 
