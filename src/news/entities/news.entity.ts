7ea2f174594f68feccbfbc26450393c6b3f9f272
Martial59110 <floquet.martial@gmail.com>
2025-05-13 19:41:44 +0200

    fix(news): update news entity for better API compatibility

--- src/news/entities/news.entity.ts
+++ src/news/entities/news.entity.ts
@@ -7,6 +7,12 @@ export class News {
   imageUrl: string;
   createdAt: Date;
   updatedAt: Date;
-  userId: string;
-  user?: User;
+  userId: string | null;
+  user: {
+    id: string;
+    email: string;
+    firstname: string | null;
+    lastname: string | null;
+    roles: string[];
+  } | null;
 } 
