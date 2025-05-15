c198fe6c6ccf6bedd0b90fda5b6de4461e66f482
Martial59110 <floquet.martial@gmail.com>
2025-05-15 20:41:23 +0200

    feat(prisma): add boutique images migration

---
+++ prisma/migrations/20250515172444_add_images_boutique/migration.sql
@@ -0,0 +1,12 @@
+-- AlterTable
+ALTER TABLE "boutique" ADD COLUMN     "image10" TEXT,
+ADD COLUMN     "image11" TEXT,
+ADD COLUMN     "image12" TEXT,
+ADD COLUMN     "image13" TEXT,
+ADD COLUMN     "image14" TEXT,
+ADD COLUMN     "image4" TEXT,
+ADD COLUMN     "image5" TEXT,
+ADD COLUMN     "image6" TEXT,
+ADD COLUMN     "image7" TEXT,
+ADD COLUMN     "image8" TEXT,
+ADD COLUMN     "image9" TEXT;
