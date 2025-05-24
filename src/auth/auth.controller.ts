d37ed2a63e0767298765a2da5be2df32ef82d8a0
Martial59110 <floquet.martial@gmail.com>
2025-05-24 14:53:20 +0200

    feat(auth): update auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Req, Patch } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { Public } from '../decorators/public.decorator';
+import { ForgotPasswordDto } from './dto/forgot-password.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -34,5 +35,17 @@ export class AuthController {
   async logout(@Req() req: Request) {
     const refreshToken = req.body.refresh_token;
     return this.authService.logout(refreshToken);
+  }
+
+  @Public()
+  @Post('forgot-password')
+  async forgotPassword(@Body() dto: ForgotPasswordDto) {
+    return this.authService.forgotPassword(dto.email);
+  }
+
+  @Patch('reset-password')
+  @Public()
+  async resetPassword(@Body() body: { token: string, password: string }) {
+    return this.authService.resetPassword(body.token, body.password);
   }
 } 
