2bdcc961cd0cd523d7db8ab4148b9476f3360ed5
Martial59110 <floquet.martial@gmail.com>
2025-05-24 14:53:21 +0200

    feat(app): update main configuration

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,29 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
   app.enableCors();
+  
+  // Ajout du middleware pour logger le corps de la requête
+  app.use(express.json({ limit: '50mb' }));
+  app.use((req, res, next) => {
+    if (req.method === 'PATCH' || req.method === 'POST') {
+      console.log('Corps de la requête:', JSON.stringify(req.body, null, 2));
+    }
+    next();
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
   const configService = app.get(ConfigService);
   console.log('Static assets path:', join(process.cwd(), 'public', 'assets'));
-  app.use('/assets', express.static(join(process.cwd(), 'public', 'assets')));
+  app.use('/assets', express.static(join(process.cwd(), 'public', 'assets'), {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.pdf')) {
+        res.setHeader('Content-Disposition', 'attachment');
+      }
+    }
+  }));
   await app.listen(configService.get('PORT', 3001));
 }
 bootstrap();
